ca7e84213391012251b3df33a59af37dc502e99b
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2016-10-30 09:43:16 -0500

    Add files via upload

---
+++ IProduct.kt
@@ -0,0 +1,8 @@
+package com.emedinaa.patterns
+
+/**
+ * Created by eduardo on 30/10/16.
+ */
+interface IProduct {
+    fun manipulate()
+}
---
+++ Product2.kt
@@ -0,0 +1,11 @@
+package com.emedinaa.patterns
+
+/**
+ * Created by eduardo on 30/10/16.
+ */
+class Product2 :IProduct{
+    override fun manipulate() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("Product 2")
+    }
+}
